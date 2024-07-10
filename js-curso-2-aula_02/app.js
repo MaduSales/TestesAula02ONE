632d1718ea499ef63bcea3680a0552e13231ee68
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2024-07-09 23:36:25 -0300

    Add files via upload

---
+++ js-curso-2-aula_02/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 0';
+
+function verificarChute(){
+    //Função: Trecho de código responsável por uma ação específica;
+    console.log('O botão foi clicado!')
+}
